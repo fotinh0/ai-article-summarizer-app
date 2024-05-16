e154212a8d10c04283084d7ee08cedc7c5a00abb
Foti Ceci <foti.ceci@gmail.com>
2024-05-16 16:32:11 -0400

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,8 +23,8 @@ const Hero = () => {
       </nav>
 
       <h1 className="head_text">
-        Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient ">OpenAI GPT-4</span>
+        Summarize Articles Using
+        <span className="orange_gradient "> AI</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer
